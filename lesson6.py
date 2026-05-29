d4cb201f720871d2c6f49ffdc2963b11727743fb
LetoKel <letovgorsok@gmail.com>
2026-05-29 16:21:45 +0300

    Add files via upload

---
+++ lesson6.py
@@ -0,0 +1,44 @@
+password = input("Введите пароль: ")
+
+def is_very_long(password):
+	password_length = len(password)
+	if password_length > 12:
+		return True
+	if password_length <=12:
+		return False
+
+
+def has_digit(password):
+	return any(letter.isdigit() for letter in password)
+
+
+def has_upper_letters(password):
+	return any(letter.isupper() for letter in password)
+
+
+def has_lower_letters(password):
+	return any(letter.islower() for letter in password)
+
+
+def has_symbols (password):
+	for letter in password:
+		if not letter.isdigit() and not letter.isalpha():
+			return True
+
+
+password_checks = [is_very_long, has_digit, has_upper_letters, has_lower_letters, has_symbols]
+
+
+def count_score(password_checks, password):
+	current_score = 0
+	for password_check in password_checks:
+		if password_check(password):
+			current_score = current_score + 2
+	return current_score
+
+
+score = count_score(password_checks, password)
+
+print("Рейтинг пароля:", score)
+
+
